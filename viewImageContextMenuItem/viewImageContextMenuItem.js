be1a1ec98135d54cd81231ce43406444f2361a16
Anton Goupinets <agoupinets@gmail.com>
2021-04-24 14:42:06 -0400

    Version 1.1

--- viewImageContextMenuItem/viewImageContextMenuItem.js
+++ viewImageContextMenuItem/viewImageContextMenuItem.js
@@ -1,12 +1,13 @@
 browser.menus.create({
   id: "view-image-context-menu-item",
   contexts: ["image"],
-  title: "View &Image"
-}, function() { if (browser.runtime.lastError) { console.log(`Error: ${browser.runtime.lastError}`); } });
-
+  title: "View I&mage"
+});
 
 browser.menus.onClicked.addListener(function(info, tab) {
-  if (info.menuItemId != "view-image-context-menu-item") { return }
+  if (info.menuItemId != "view-image-context-menu-item") {
+    return;
+  }
 
   if (info.button === 1) { // middle click
     browser.tabs.create({ url: info.srcUrl, openerTabId: tab.id });
