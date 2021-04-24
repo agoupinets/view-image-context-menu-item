31831f7ffe7276c3dc5e97389c7da59f541017c5
Anton Goupinets <agoupinets@gmail.com>
2021-04-24 14:42:20 -0400

    Version 1.2

--- viewImageContextMenuItem/viewImageContextMenuItem.js
+++ viewImageContextMenuItem/viewImageContextMenuItem.js
@@ -9,9 +9,35 @@ browser.menus.onClicked.addListener(function(info, tab) {
     return;
   }
 
-  if (info.button === 1) { // middle click
-    browser.tabs.create({ url: info.srcUrl, openerTabId: tab.id });
-  } else {
+  const isMiddleClick = info.button === 1;
+  const isShiftModifier = info.modifiers.indexOf("Shift") > -1
+  const isCtrlModifier = info.modifiers.indexOf("Ctrl") > -1
+
+  function openInSameTab(info, tab) {
     browser.tabs.update(tab.id, { url: info.srcUrl });
+  }
+
+  function openInNewForegroundTab(info, tab) {
+    browser.tabs.create({ url: info.srcUrl, openerTabId: tab.id, active: true });
+  }
+
+  function openInNewBackgroundTab(info, tab) {
+    browser.tabs.create({ url: info.srcUrl, openerTabId: tab.id, active: false });
+  }
+
+  function openInNewWindow(info, tab) {
+    browser.windows.create({ url: info.srcUrl, focused: true });
+  }
+
+  if (isMiddleClick) {
+    openInNewForegroundTab(info, tab);
+  } else if(isShiftModifier && isCtrlModifier) {
+    openInNewBackgroundTab(info, tab);
+  } else if(isShiftModifier) {
+    openInNewWindow(info, tab);
+  } else if(isCtrlModifier) {
+    openInNewForegroundTab(info, tab);
+  } else {
+    openInSameTab(info, tab);
   }
 });
