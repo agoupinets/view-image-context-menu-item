7b2f0dc774e725f38afeed0265162965f3200168
Anton Goupinets <agoupinets@gmail.com>
2024-10-15 17:52:59 -0400

    add blob and data scheme support + refactor

--- viewImageContextMenuItem/background.js
+++ viewImageContextMenuItem/background.js
@@ -15,38 +15,28 @@ window.DEFAULT_OPTIONS = Object.freeze({
   });
 
 function loadOptionsFromStorage() {
-  return browser.storage.local.get("options")
-    .then((resolve, reject) => {
-      const options = resolve.options || {};
-      return new Promise((resolve, reject) => {
-        Object.keys(window.DEFAULT_OPTIONS).forEach((key) => {
-          if(!options.hasOwnProperty(key)) {
-            options[key] = window.DEFAULT_OPTIONS[key];
-          }
-        });
-        resolve(options);
-      });
+  return browser.storage.local.get("options").then((resolve, reject) => {
+    const options = resolve.options || {};
+    return new Promise((resolve, reject) => {
+      Object.keys(window.DEFAULT_OPTIONS).forEach((key) => { if (!options.hasOwnProperty(key)) options[key] = window.DEFAULT_OPTIONS[key]; });
+      resolve(options);
     });
+  });
 }
 
-function createMenuItems() {
-  browser.menus.create({
-    id: "view-audio-context-menu-item",
-    contexts: ["audio"],
-    title: browser.i18n.getMessage("menuItemViewAudio")
-  });
+function handleStorageChange(changes, areaName) {
+  if (areaName !== "local" || !changes.options) return;
+  applyOptions(changes.options.newValue);
+}
 
-  browser.menus.create({
-    id: "view-image-context-menu-item",
-    contexts: ["image"],
-    title: browser.i18n.getMessage("menuItemViewImage")
-  });
+function applyOptions(options) {
+  window.options = options;
+}
 
-  browser.menus.create({
-    id: "view-video-context-menu-item",
-    contexts: ["video"],
-    title: browser.i18n.getMessage("menuItemViewVideo")
-  });
+function loadOptionsFromStorageMessageListener(request, sender, sendResponse) {
+  if (request && request.getOptions === true) {
+    sendResponse(loadOptionsFromStorage());
+  }
 }
 
 function applyActionKeyToMenuItemTitle(defaultMenuItemTitle, actionKey){
@@ -90,98 +80,77 @@ function handleContextMenuShow(info, tab) {
 function handleContextMenuItemClick(info, tab) {
   if (info.menuItemId != "view-audio-context-menu-item" &&
       info.menuItemId != "view-image-context-menu-item" &&
-      info.menuItemId != "view-video-context-menu-item") {
-    return;
-  }
+      info.menuItemId != "view-video-context-menu-item") return;
 
-  function getMediaType(info) {
+  const mediaType = ((info) => {
     switch(info.menuItemId) {
-      case "view-audio-context-menu-item":
-        return "audio";
-      case "view-image-context-menu-item":
-        return "image";
-      case "view-video-context-menu-item":
-        return "video";
-      default:
-        return null;
+      case "view-audio-context-menu-item": return "audio";
+      case "view-image-context-menu-item": return "image";
+      case "view-video-context-menu-item": return "video";
+      default: return null;
     }
-  }
+  })(info);
 
-  function getActionOptionType(info) {
+  const clickType = ((info) => {
     const isMiddleClick = info.button === 1;
     const isShiftModifier = info.modifiers.indexOf("Shift") > -1;
     const isCtrlModifier = info.modifiers.indexOf("Ctrl") > -1;
-    if (isMiddleClick) {
-      return "middle-click-action";
-    } else if(isShiftModifier && isCtrlModifier) {
-      return "ctrl-shift-left-click-action";
-    } else if(isShiftModifier) {
-      return "shift-left-click-action";
-    } else if(isCtrlModifier) {
-      return "ctrl-left-click-action";
-    } else {
-      return "left-click-action";
-    }
-  }
+    if (isMiddleClick) return "middle-click-action";
+    if (isShiftModifier && isCtrlModifier) return "ctrl-shift-left-click-action";
+    if (isShiftModifier) return "shift-left-click-action";
+    if (isCtrlModifier) return "ctrl-left-click-action";
+    return "left-click-action";
+  })(info);
+  const actionType = window.options[clickType];
 
-  function isFileScheme(url) {
-    return url.match(/^file:\/\//gi) !== null;
-  }
+  function isFileScheme(url) { return url.match(/^file:\/\//gi) !== null; }
+  function isDataScheme(url) { return url.match(/^data:/gi) !== null; }
+  function isBlobScheme(url) { return url.match(/^blob:/gi) !== null; }
+  const usePrivilegedUrlWorkaround = 
+    (isFileScheme(info.srcUrl) && isFileScheme(info.pageUrl)) // both page and image url are local "file://" scheme
+    || isDataScheme(info.srcUrl) // image url is a "data:" scheme
+    || isBlobScheme(info.srcUrl) // image url is a "blob:" scheme
+  ;
 
-  const mediaType = getMediaType(info);
-  const actionType = window.options[getActionOptionType(info)];
-  const isLocalFileLink = isFileScheme(info.pageUrl) && isFileScheme(tab.url);
-
-  if(isLocalFileLink) {
-    processViewActionLocalFile(info, tab, actionType);
+  if (usePrivilegedUrlWorkaround) {
+    processActionViaAnchorClick(info, tab, actionType);
   } else {
-    processViewAction(info, tab, actionType, mediaType);
+    processAction(info, tab, actionType, mediaType);
   }
 }
 
-function processViewAction(info, tab, actionType, mediaType) {
-  function getRefererFromInfo(info) {
-    return (info.pageUrl.match(/^[^\/]+:\/\/[^\/]+\//g) || [null])[0];
-  }
-
-  function getAcceptValue(mediaType) {
-    switch(mediaType) {
-      case "audio":
-        return "audio/webm,audio/ogg,audio/wav,audio/*;q=0.9,application/ogg;q=0.7,video/*;q=0.6,*/*;q=0.5";
-      case "image":
-        return "image/avif,image/webp,*/*";
-      case "video":
-        return "video/webm,video/ogg,video/*;q=0.9,application/ogg;q=0.7,audio/*;q=0.6,*/*;q=0.5";
-      default:
-        return null;
+function setRequestHeader(headers, headerName, value) {
+  for (let header of headers) {
+    if (header.name.toLowerCase() === headerName.toLowerCase()) {
+      header.value = value;
+      return;
     }
   }
+  headers.push({ name: headerName, value: value });
+}
 
-  function setHeader(headers, headerName, value) {
-    for (let header of headers) {
-      if (header.name.toLowerCase() === headerName.toLowerCase()) {
-        header.value = value;
-        return;
-      }
-    }
-    headers.push({ name: headerName, value: value });
-  }
+function processAction(info, tab, actionType, mediaType) {
+  const destinationUrl = info.srcUrl;
 
-  function interceptRequestAndSetHeaders(destinationUrl, info, mediaType) {
-    const setReferer = window.options["override-referer"] === true;
-    const setAccept = window.options["override-accept"] === true;
-
+  if (window.options["override-referer"] === true || window.options["override-accept"] === true) {
     const listener = function(event) {
       browser.webRequest.onBeforeSendHeaders.removeListener(listener);
       return new Promise((resolve, reject) => {
-        if (setReferer) {
-          const refererUrl = getRefererFromInfo(info);
-          setHeader(event.requestHeaders, "Referer", refererUrl);
+        if (window.options["override-referer"] === true) {
+          const refererValue = (info.pageUrl.match(/^[^\/]+:\/\/[^\/]+\//g) || [null])[0];
+          setRequestHeader(event.requestHeaders, "Referer", refererValue);
         }
-        if (setAccept) {
-          const acceptValue = getAcceptValue(mediaType);
+        if (window.options["override-accept"] === true) {
+          const acceptValue = ((mediaType) => {
+            switch(mediaType) {
+              case "audio": return "audio/webm,audio/ogg,audio/wav,audio/*;q=0.9,application/ogg;q=0.7,video/*;q=0.6,*/*;q=0.5";
+              case "image": return "image/avif,image/webp,*/*";
+              case "video": return "video/webm,video/ogg,video/*;q=0.9,application/ogg;q=0.7,audio/*;q=0.6,*/*;q=0.5";
+              default: return null;
+            }
+          })(mediaType);
           if (acceptValue !== null) {
-            setHeader(event.requestHeaders, "Accept", acceptValue);
+            setRequestHeader(event.requestHeaders, "Accept", acceptValue);
           }
         }
 
@@ -189,45 +158,29 @@ function processViewAction(info, tab, actionType, mediaType) {
       });
     };
 
-    browser.webRequest.onBeforeSendHeaders.addListener(
-      listener,
-      { urls: [destinationUrl] },
-      ["blocking", "requestHeaders"]
-    );
+    browser.webRequest.onBeforeSendHeaders.addListener(listener, { urls: [destinationUrl] }, ["blocking", "requestHeaders"]);
   }
 
-  function prepareForRequest(info, mediaType) {
-    const destinationUrl = info.srcUrl;
-    if (window.options["override-referer"] === true || window.options["override-accept"] === true) {
-      interceptRequestAndSetHeaders(destinationUrl, info, mediaType);
-    }
-    return destinationUrl;
+  switch(actionType) {
+    case "same-tab":
+      browser.tabs.update(tab.id, { url: destinationUrl });
+      break;
+    case "new-foreground-tab":
+      browser.tabs.create({ url: destinationUrl, openerTabId: tab.id, active: true });
+      break;
+    case "new-background-tab":
+      browser.tabs.create({ url: destinationUrl, openerTabId: tab.id, active: false });
+      break;
+    case "new-foreground-window":
+      browser.windows.create({ url: destinationUrl, focused: true, incognito: tab.incognito });
+      break;
   }
-
-  function doAction(info, tab, actionType, mediaType){
-    const destinationUrl = prepareForRequest(info, mediaType);
-    switch(actionType) {
-      case "same-tab":
-        browser.tabs.update(tab.id, { url: destinationUrl });
-        break;
-      case "new-foreground-tab":
-        browser.tabs.create({ url: destinationUrl, openerTabId: tab.id, active: true });
-        break;
-      case "new-background-tab":
-        browser.tabs.create({ url: destinationUrl, openerTabId: tab.id, active: false });
-        break;
-      case "new-foreground-window":
-        browser.windows.create({ url: destinationUrl, focused: true, incognito: tab.incognito });
-        break;
-    }
-  }
-
-  doAction(info, tab, actionType, mediaType);
 }
 
-// Firefox security policy blocks most file:// actions, so this code
-// jumps through a lot of hoops to get around it
-function processViewActionLocalFile(info, tab, actionType) {
+// Firefox security policy blocks lot of actions for "privileged urls"
+// https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/tabs/update#url
+// this code jumps through a lot of hoops to get around it by creating and click an achor tag
+function processActionViaAnchorClick(info, tab, actionType) {
   function redirectTabToUrl(tabId, url) {
     browser.tabs.executeScript(
       tabId,
@@ -243,62 +196,43 @@ function processViewActionLocalFile(info, tab, actionType) {
 
   function createListenerToRedirectTabToUrl(tabId, url){
     const listener = function (listenerTabId, changeInfo, tabInfo) {
-      if(listenerTabId !== tabId || changeInfo.status !== "complete") {
-        return;
-      }
+      if (listenerTabId !== tabId || changeInfo.status !== "complete") return;
       browser.tabs.onUpdated.removeListener(listener);
       redirectTabToUrl(tabId, url);
     };
     browser.tabs.onUpdated.addListener(listener);
   }
 
-  function doAction(info, tab, actionType){
-    const destinationUrl = info.srcUrl;
-    switch(actionType) {
-      case "same-tab":
-        redirectTabToUrl(tab.id, destinationUrl);
-        break;
-      case "new-foreground-tab":
-        browser.tabs.duplicate(tab.id, { active: true })
-          .then(function (newTab) { createListenerToRedirectTabToUrl(newTab.id, destinationUrl); });
-        break;
-      case "new-background-tab":
-        browser.tabs.duplicate(tab.id, { active: false })
-          .then(function (newTab) { createListenerToRedirectTabToUrl(newTab.id, destinationUrl); });
-        break;
-      case "new-foreground-window":
-        browser.tabs.duplicate(tab.id, { active: false })
-          .then(function (newTab) { 
-            browser.windows.create({ tabId: newTab.id, focused: true, incognito: newTab.incognito });
-            createListenerToRedirectTabToUrl(newTab.id, destinationUrl);
-          });
-        break;
-    }
-  }
-
-  doAction(info, tab, actionType);
-}
-
-function handleStorageChange(changes, areaName){
-  if(areaName !== "local" || !changes.options) {
-    return;
-  }
-  processOptions(changes.options.newValue);
-}
-
-function processOptions(options) {
-  window.options = options;
-}
-
-function loadOptionsFromStorageMessageListener(request, sender, sendResponse) {
-  if(request && request.getOptions === true) {
-    sendResponse(loadOptionsFromStorage());
+  const destinationUrl = info.srcUrl;
+  switch(actionType) {
+    case "same-tab":
+      redirectTabToUrl(tab.id, destinationUrl);
+      break;
+    case "new-foreground-tab":
+      browser.tabs.duplicate(tab.id, { active: true })
+        .then(function (newTab) { createListenerToRedirectTabToUrl(newTab.id, destinationUrl); });
+      break;
+    case "new-background-tab":
+      browser.tabs.duplicate(tab.id, { active: false })
+        .then(function (newTab) { createListenerToRedirectTabToUrl(newTab.id, destinationUrl); });
+      break;
+    case "new-foreground-window":
+      browser.tabs.duplicate(tab.id, { active: false })
+        .then(function (newTab) { 
+          browser.windows.create({ tabId: newTab.id, focused: true, incognito: newTab.incognito });
+          createListenerToRedirectTabToUrl(newTab.id, destinationUrl);
+        });
+      break;
   }
 }
 
-createMenuItems();
-processOptions(window.DEFAULT_OPTIONS);
-loadOptionsFromStorage().then((options) => { processOptions(options); });
+browser.menus.create({ id: "view-audio-context-menu-item", contexts: ["audio"], title: browser.i18n.getMessage("menuItemViewAudio") });
+browser.menus.create({ id: "view-image-context-menu-item", contexts: ["image"], title: browser.i18n.getMessage("menuItemViewImage") });
+browser.menus.create({ id: "view-video-context-menu-item", contexts: ["video"], title: browser.i18n.getMessage("menuItemViewVideo") });
+
+applyOptions(window.DEFAULT_OPTIONS);
+loadOptionsFromStorage().then((options) => { applyOptions(options); });
+
 browser.menus.onShown.addListener(handleContextMenuShow);
 browser.menus.onClicked.addListener(handleContextMenuItemClick);
 browser.storage.onChanged.addListener(handleStorageChange);
